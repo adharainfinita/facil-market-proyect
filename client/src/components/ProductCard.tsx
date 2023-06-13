5c5a8a128870e2c1711e330f8a0e19241de0ae61
Laura Perez <laucatape@gmail.com>
2023-06-13 18:17:03 -0500

    move information to another branch

--- client/src/components/ProductCard.tsx
+++ client/src/components/ProductCard.tsx
@@ -11,21 +11,18 @@ type productList = {
     products: Array<Product>
 }
 
-
 function ProductCard(props: productList) {
     const products =  props.products
     return(
        <>
        {products.map((product) => {
         return(
-        <div key={product.id}>
+        <div key={product.id} className="product-card">
             <img src={product.image} alt={product.title} />
             <div>
                 <h3>{product.title}</h3>
                 <p>Location</p>
-                <div>
-                    <p>{product.category}</p>
-                </div>
+                <p>{product.category}</p>
                 <h4>{product.price}</h4>
             </div>
         </div>
