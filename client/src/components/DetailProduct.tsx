10d2cea575dc28b9c5c70047987e3f7bf79baf5d
Laura Perez <laucatape@gmail.com>
2023-06-15 23:36:03 -0500

    fix(client/detail) repair className errors

--- client/src/components/DetailProduct.tsx
+++ client/src/components/DetailProduct.tsx
@@ -1,25 +1,23 @@
-
-
 import useProduct from "../hooks/useProduct";
 
 const DetailProduct = () => {
   const product = useProduct()
    
     return (
-        <div>
-            <div>
-                <div>
+        <div className="detail-product-container">
+            <div className="detail-product">
+                <div className="detail-product-image">
                     <img src={product?.image} alt={product.name} />
                 </div>
-                <h1>{product?.name}</h1>
-                <h1>{product?.price}</h1>
-                <div>
-                    <h2>{product.userName}</h2>
-                    <h2>{product.nameCategory}</h2>
+                <h1 className="detail-product-name">{product?.name}</h1>
+                <h1 className="detail-product-price">{product?.price}</h1>
+                <div className="detail-product-info">
+                    <h2 className="detail-product-seller">{product.userName}</h2>
+                    <h2 className="detail-product-category">{product.categoryName}</h2>
                     <h2>{product.stock}</h2>
                     <h2>{product.rating}</h2>
                     <h2>{product.location}</h2>
-                    <p>{product.description}</p>
+                    <p className="detail-product-description">{product.description}</p>
                 </div>
             </div>
         </div>
