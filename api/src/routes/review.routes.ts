605d6e02c61ec236d576c0cf287d74689018fc6a
Glutix <ferreyra.gluti@gmail.com>
2023-06-26 20:31:06 -0300

    fix/modulesRoutes

---
+++ api/src/routes/review.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+const review = Router();
+
+import postReview from "../handlers/reviews/postReview";
+import getAllReviews from "../handlers/reviews/getReviews";
+import { reviewCreate } from "../validators/reviewValidation";
+
+//! routes Create
+review.post("/", reviewCreate, postReview);
+
+//! routes Read
+review.get("/", getAllReviews);
+
+//! routes Update
+//! routes Delete
+export default review;
---
+++ api/src/routes/users.routes.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+const user = Router();
+
+import { getUserById, getAllUsers } from "../handlers/auth/getUsers";
+
+import updateUser from "../handlers/auth/updateUser";
+
+//! routes Create
+
+//! routes Read
+user.get("/", getAllUsers);
+user.get("/:userId", getUserById);
+
+//! routes Update
+user.put("/:userId", updateUser);
+
+//! routes Delete
+
+export default user;
