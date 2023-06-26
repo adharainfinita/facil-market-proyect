b7399d3746b0ddcafc3f8e4cf0e7da72e46d878a
Adhara Redruello <113715775+adharainfinita@users.noreply.github.com>
2023-06-26 08:25:34 -0300

    Update webhook.ts

--- api/src/handlers/payments/webhook.ts
+++ api/src/handlers/payments/webhook.ts
@@ -8,12 +8,12 @@ const receivedWebhook = async (req: Request, res: Response) => {
 		if (payment.type === "payment") {
 			const id = Number(payment["data.id"]);
 			const data = await mercadopago.payment.findById(id);
-		
+			console.log(data)
 			//Podría enviarse la información del recibo de pago para enviarselo por email al comprador
 			// const response = await Payments.create({
 			// se guarda la información de la compra en la db para pagar posteriormente
 			// })
-			return res.status(204).json(data);
+			return res.sendStatus(204)
 		}
 	} catch (error: any) {
 		console.log(error);
