9181c98276ff0c32d83eeb4f5559e8a90f01b21c
Cachilox <marianopvp1@gmail.com>
2023-06-29 15:56:06 -0300

    feat(client/protectedRoute): add protected routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,30 @@
+import axios from "axios";
 const URL_HOST = import.meta.env.VITE_HOST;
+import { useEffect } from "react";
 import { Route, Routes, Link } from "react-router-dom";
-import Terms from "./pages/Terms";
-import Navbar from "./components/Navbar";
-import FormCreateProduct from "./components/FormCreateProduct";
-import Footer from "./components/Footer";
-import VerificationPage from "./pages/VerificationPage";
+import { useDispatch, useSelector } from "react-redux";
+// Pages
 import Login from "./pages/Login";
+import Terms from "./pages/Terms";
+import VerificationPage from "./pages/VerificationPage";
 import Home from "./pages/Home";
-import RegisterForm from "./components/RegisterForm";
-import DetailProduct from "./components/DetailProduct";
 import Market from "./pages/Market";
 import UserProfile from "./pages/UserProfile";
 import UserProducts from "./pages/UserProducts";
+// Dashboard Admin
+import Dashboard from "./pages/admin/Dashboard";
+import Users from "./pages/admin/Users";
+import Products from "./pages/admin/Products";
+import EditUser from "./pages/admin/EditUser";
+// Components
+import Navbar from "./components/Navbar";
+import FormCreateProduct from "./components/FormCreateProduct";
+import Footer from "./components/Footer";
+import RegisterForm from "./components/RegisterForm";
+import DetailProduct from "./components/DetailProduct";
+import ProtectedRoute from "./components/ProtectedRoute";
+// redux/services
+import { RootState } from "./redux/store";
 import { getAllProducts } from "./services/productServices";
 import { getProducts } from "./redux/features/productSlice";
 import {
@@ -22,23 +35,11 @@ import {
   changeName,
   changeImage,
 } from "./redux/features/userSlice";
-import { getAllUsers } from "./services/userServices";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { getAllUsers, getUserById } from "./services/userServices";
 import { getCategories } from "./redux/features/categorySlice";
 import { getCategory } from "./services/categoryServices";
-import { RootState } from "./redux/store";
-import { useSelector } from "react-redux/es/hooks/useSelector";
-import { getUserById } from "./services/userServices";
-import axios from "axios";
 
-// Dashboard Admin
-import Dashboard from "./pages/admin/Dashboard";
-import Users from "./pages/admin/Users";
-import Products from "./pages/admin/Products";
-import EditUser from "./pages/admin/EditUser";
-
-function App() {
+const App = () => {
   const dispatch = useDispatch();
   const session = window.localStorage.getItem("token");
 
@@ -143,46 +144,41 @@ function App() {
     <>
       <Navbar />
 
-      {session ? (
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/vender" element={<FormCreateProduct />} />
-          <Route path="/terminos_y_condiciones" element={<Terms />} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/vender" element={<FormCreateProduct />} />
+        <Route path="/terminos_y_condiciones" element={<Terms />} />
+
+        <Route element={<ProtectedRoute isAllowed={Boolean(session)} />}>
           <Route path="/profile" element={<UserProfile />} />
           <Route path="/ventas" element={<UserProducts />} />
           <Route path="/verification" element={<VerificationPage />} />
-          <Route path="/products" element={<Market />} />
-          <Route path="/product/detail/:id" element={<DetailProduct />} />
           <Route path="/user/:id" element={<EditUser />} />
 
           <Route path="/admin" element={<Dashboard />}>
             <Route path="users" element={<Users />} />
             <Route path="products" element={<Products />} />
           </Route>
-        </Routes>
-      ) : (
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/vender" element={<FormCreateProduct />} />
-          <Route path="/terminos_y_condiciones" element={<Terms />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<RegisterForm />} />
-          <Route path="/products" element={<Market />} />
-          <Route path="/product/detail/:id" element={<DetailProduct />} />
-          <Route
-            path="*"
-            element={
-              <h1>
-                404 no hay nada loco Go <Link to="/">home</Link>
-              </h1>
-            }
-          />
-        </Routes>
-      )}
+        </Route>
+
+        <Route path="/products" element={<Market />} />
+        <Route path="/product/detail/:id" element={<DetailProduct />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<RegisterForm />} />
+
+        <Route
+          path="*"
+          element={
+            <h1>
+              404 no hay nada loco Go <Link to="/">home</Link>
+            </h1>
+          }
+        />
+      </Routes>
 
       <Footer />
     </>
   );
-}
+};
 
 export default App;
