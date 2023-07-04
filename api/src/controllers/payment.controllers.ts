6ef485e581173dbcb55783c5def873518d72bd1a
Adhara Redruello <113715775+adharainfinita@users.noreply.github.com>
2023-07-04 03:04:16 -0300

    Update payment.controllers.ts

--- api/src/controllers/payment.controllers.ts
+++ api/src/controllers/payment.controllers.ts
@@ -58,7 +58,7 @@ export const createOrder = async (products: Array<BuyProduct>, userID:number) =>
 			email: userInfo?.email,
 			phone: {
 				area_code: "54",
-				number: 3644123456,
+				number: '3644123456',
 			},
 			identification: {
 				type: "DNI",
