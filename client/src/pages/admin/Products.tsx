0eb7203dbf2ca704ad58cbd9f8b17c6706653a8f
Glutix <ferreyra.gluti@gmail.com>
2023-07-04 14:42:25 -0300

    fix/desactivar-product

--- client/src/pages/admin/Products.tsx
+++ client/src/pages/admin/Products.tsx
@@ -1,18 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
 import { deleteProduct } from "../../services/productServices";
+import { Product } from "../../utils/interfaces";
 
 function Products() {
 	const products = useSelector((state: RootState) => state.product.products);
-	const [active, setActive] = useState<boolean>(true);
+	const [updatedProducts, setUpdatedProducts] = useState<Product[]>([]);
 
-	const disabledProduct = async (productID: number) => {
-		const disabled = await deleteProduct(productID);
-		console.log(disabled);
-
-		setActive(!active);
+	const disabledProduct = async (product: Product) => {
+		const disabled = await deleteProduct(product.id);
+		const updatedProductsList = updatedProducts.map((p) => {
+			if (p.id === product.id) {
+				return { ...p, active: disabled.prod.active };
+			}
+			return p;
+		});
+		setUpdatedProducts(updatedProductsList);
 	};
+
+	useEffect(() => {
+		setUpdatedProducts(products);
+	}, [products]);
 
 	return (
 		<table className="user-table">
@@ -27,7 +36,7 @@ function Products() {
 				</tr>
 			</thead>
 			<tbody>
-				{products.map((product) => (
+				{updatedProducts.map((product) => (
 					<tr key={product.id}>
 						<td className="user-table-td">{product.id}</td>
 						<td className="user-table-td">
@@ -37,8 +46,8 @@ function Products() {
 						<td className="user-table-td">${product.price}</td>
 						<td className="user-table-td">{product.unities}</td>
 						<td className="user-table-td">
-							<button onClick={() => disabledProduct(product.id)}>
-								{active ? "Desactivar" : "Activar"}
+							<button onClick={() => disabledProduct(product)}>
+								{product.active ? "Desactivar" : "Activar"}
 							</button>
 						</td>
 					</tr>
