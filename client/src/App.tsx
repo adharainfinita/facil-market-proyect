2fea873e484ffdde59f5f2e9aa2a2f65025ebbef
Adhara <adharanosalevich@gmail.com>
2023-07-04 02:23:36 -0300

    'merg ale'

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -50,7 +50,6 @@ import {
 import { getAllUsers, getUserById } from "./services/userServices";
 import { getCategories } from "./redux/features/categorySlice";
 import { getCategory } from "./services/categoryServices";
-import ProductEdit from "./components/ProductEdit";
 import { createCart, getAllItems } from "./services/cartServicer";
 import ProductEdit from "./components/ProductEdit";
 import { startCart } from "./redux/features/cartSlice";
