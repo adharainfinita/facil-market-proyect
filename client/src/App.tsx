e041a5f21a551c3548bea4342a191c74de4c0914
Cachilox <marianopvp1@gmail.com>
2023-07-04 00:00:55 -0300

    feat/navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -40,12 +40,12 @@ import { RootState } from "./redux/store";
 import { getAllProducts } from "./services/productServices";
 import { getProducts } from "./redux/features/productSlice";
 import {
-	changePassword,
-	getUsers,
-	userLogin,
-	changeEmail,
-	changeName,
-	changeImage,
+  changePassword,
+  getUsers,
+  userLogin,
+  changeEmail,
+  changeName,
+  changeImage,
 } from "./redux/features/userSlice";
 import { getAllUsers, getUserById } from "./services/userServices";
 import { getCategories } from "./redux/features/categorySlice";
@@ -54,12 +54,12 @@ import { getAllItems } from "./services/cartServicer";
 import ProductEdit from "./components/ProductEdit";
 
 const App = () => {
-	const dispatch = useDispatch();
-	const session = window.localStorage.getItem("token");
+  const dispatch = useDispatch();
+  const session = window.localStorage.getItem("token");
 
-	const headers = {
-		Authorization: `Bearer ${session}`,
-	};
+  const headers = {
+    Authorization: `Bearer ${session}`,
+  };
 
   const login = useSelector((state: RootState) => state.user.userLogin);
   const permissions = login?.user?.admin;
@@ -71,157 +71,149 @@ const App = () => {
       const userId = id; // Reemplaza con el ID del usuario deseado
       const fetchedUser = await getUserById(userId);
 
-			if (fetchedUser) {
-				if (fetchedUser.image !== undefined) {
-					const newImg = fetchedUser.image;
-					dispatch(changeImage(newImg));
-				}
-				if (fetchedUser.fullName !== undefined) {
-					const newName = fetchedUser.fullName;
-					dispatch(changeName(newName));
-				}
-				if (fetchedUser.email !== undefined) {
-					const newEmail = fetchedUser.email.toString(); // Convertir a cadena
-					dispatch(changeEmail(newEmail));
-				}
-				if (fetchedUser.password !== undefined) {
-					const newPassword = fetchedUser.password.toString(); // Convertir a cadena
-					dispatch(changePassword(newPassword));
-				}
-			}
-		};
+      if (fetchedUser) {
+        if (fetchedUser.image !== undefined) {
+          const newImg = fetchedUser.image;
+          dispatch(changeImage(newImg));
+        }
+        if (fetchedUser.fullName !== undefined) {
+          const newName = fetchedUser.fullName;
+          dispatch(changeName(newName));
+        }
+        if (fetchedUser.email !== undefined) {
+          const newEmail = fetchedUser.email.toString(); // Convertir a cadena
+          dispatch(changeEmail(newEmail));
+        }
+        if (fetchedUser.password !== undefined) {
+          const newPassword = fetchedUser.password.toString(); // Convertir a cadena
+          dispatch(changePassword(newPassword));
+        }
+      }
+    };
 
-		fetchUserData();
-	}, [dispatch, id]);
+    fetchUserData();
+  }, [dispatch, id]);
 
-	useEffect(() => {
-		if (session) {
-			axios
-				.get(`${URL_HOST}/auth/token`, { headers })
-				.then((response) => {
-					const data = {
-						id: response.data.user.id,
-						fullName: response.data.user.fullName,
-						email: response.data.user.email,
-						image: response.data.user.image,
-						admin: true,
-					};
+  useEffect(() => {
+    if (session) {
+      axios
+        .get(`${URL_HOST}/auth/token`, { headers })
+        .then((response) => {
+          const data = {
+            id: response.data.user.id,
+            fullName: response.data.user.fullName,
+            email: response.data.user.email,
+            image: response.data.user.image,
+            admin: true,
+          };
 
-					const results = getAllItems(data.id);
-					console.log(results);
+          const results = getAllItems(data.id);
+          console.log(results);
 
-					/* const producItem: BuyProduct = {
-						id: results.id,
-						name: results.name,
-						price: results.price,
-						image: results.images[0],
-						quantity: results,
-					}; */
+          dispatch(userLogin(data));
+        })
+        .catch((error) => {
+          //? mejorar este error
+          console.log(error);
+        });
+    }
+  }, [dispatch, session]);
 
-					dispatch(userLogin(data));
-				})
-				.catch((error) => {
-					//? mejorar este error
-					console.log(error);
-				});
-		}
-	}, [dispatch, session]);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await getAllUsers();
 
-	useEffect(() => {
-		const fetchUsers = async () => {
-			try {
-				const response = await getAllUsers();
+        if (response) {
+          dispatch(getUsers(response));
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchUsers();
 
-				if (response) {
-					dispatch(getUsers(response));
-				}
-			} catch (error) {
-				console.log(error);
-			}
-		};
-		fetchUsers();
+    const fetchProducts = async () => {
+      try {
+        const response = await getAllProducts();
+        if (response) {
+          dispatch(getProducts(response));
+        } else {
+          console.error("No existen productos");
+        }
+      } catch (error) {
+        console.error("Error al obtener los productos:", error);
+      }
+    };
+    fetchProducts();
+    const fetchCategories = async () => {
+      try {
+        const response = await getCategory();
+        if (response) {
+          dispatch(getCategories(response));
+        } else {
+          console.error("No existen categorias");
+        }
+      } catch (error) {
+        console.error("Error al obtener las categorías:", error);
+      }
+    };
+    fetchCategories();
+  }, [dispatch]);
 
-		const fetchProducts = async () => {
-			try {
-				const response = await getAllProducts();
-				if (response) {
-					dispatch(getProducts(response));
-				} else {
-					console.error("No existen productos");
-				}
-			} catch (error) {
-				console.error("Error al obtener los productos:", error);
-			}
-		};
-		fetchProducts();
-		const fetchCategories = async () => {
-			try {
-				const response = await getCategory();
-				if (response) {
-					dispatch(getCategories(response));
-				} else {
-					console.error("No existen categorias");
-				}
-			} catch (error) {
-				console.error("Error al obtener las categorías:", error);
-			}
-		};
-		fetchCategories();
-	}, [dispatch]);
+  return (
+    <>
+      <Navbar />
 
-	return (
-		<>
-		<Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/vender" element={<FormCreateProduct />} />
+        <Route path="/terminos_y_condiciones" element={<Terms />} />
+        <Route path="/about" element={<About />} />
 
-		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="/vender" element={<FormCreateProduct />} />
-			<Route path="/terminos_y_condiciones" element={<Terms />} />
-			<Route path="/about" element={<About/>} />
+        <Route
+          element={
+            <ProtectedRoute isAllowed={Boolean(session)} redirectTo="/" />
+          }
+        >
+          <Route path="/profile" element={<UserProfile />} />
+          <Route path="/compras" element={<ShoppingHistory />} />
+          <Route path="/ventas" element={<UserProducts />} />
+          <Route path="/verification" element={<VerificationPage />} />
+          <Route path="/user/:id" element={<EditUser />} />
+          <Route path="/product/edit/:id" element={<ProductEdit />} />
+          <Route path="/admin" element={<Dashboard />} />
+        </Route>
 
-			<Route
-				element={
-					<ProtectedRoute isAllowed={Boolean(session)} redirectTo="/" />
-				}
-			>
-				<Route path="/profile" element={<UserProfile />} />
-				<Route path="/compras" element={<ShoppingHistory />} />
-				<Route path="/ventas" element={<UserProducts />} />
-				<Route path="/verification" element={<VerificationPage />} />
-				<Route path="/user/:id" element={<EditUser />} />
-				<Route path="/product/edit/:id" element={<ProductEdit />} />
-				<Route path="/admin" element={<Dashboard />} />
-			</Route>
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute
+              isAllowed={Boolean(session) && permissions}
+              redirectTo="/admin"
+            >
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        >
+          <Route path="summary" element={<Resume />} />
+          <Route path="users" element={<Users />} />
+          <Route path="products" element={<Products />} />
+        </Route>
 
-			<Route
-				path="/admin"
-				element={
-					<ProtectedRoute
-						isAllowed={Boolean(session) && permissions}
-						redirectTo="/admin"
-					>
-						<Dashboard />
-					</ProtectedRoute>
-				}
-			>
-				<Route path="summary" element={<Resume />} />
-				<Route path="users" element={<Users />} />
-				<Route path="products" element={<Products />} />
-			</Route>
+        <Route path="/products" element={<Market />} />
+        <Route path="/product/detail/:id" element={<DetailProduct />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<RegisterForm />} />
+        <Route path="/profile/:id" element={<UserProfiles />} />
+        <Route path="/review/:id" element={<ProductReviews />} />
 
-			<Route path="/products" element={<Market />} />
-			<Route path="/product/detail/:id" element={<DetailProduct />} />
-			<Route path="/login" element={<Login />} />
-			<Route path="/register" element={<RegisterForm />} />
-			<Route path="/profile/:id" element={<UserProfiles />} />
-			<Route path="/review/:id" element={<ProductReviews />} />
+        <Route path="/cart" element={<Cart />} />
 
-			<Route path="/cart" element={<Cart />} />
-
-			<Route path="*" element={<NotFound />} />
-		</Routes>
-		<Footer />
-	</>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </>
   );
 };
 
