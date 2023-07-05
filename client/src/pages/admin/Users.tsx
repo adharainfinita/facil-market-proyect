bf44b5f15bc869b8ef81ed5360a18fa7740a1640
Cachilox <marianopvp1@gmail.com>
2023-07-05 15:30:07 -0300

    fix/styles

--- client/src/pages/admin/Users.tsx
+++ client/src/pages/admin/Users.tsx
@@ -6,55 +6,57 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 function Users() {
-	const users = useSelector((state: RootState) => state.user.users);
-	const [updatedUsers, setUpdatedUsers] = useState<User[]>([]);
+  const users = useSelector((state: RootState) => state.user.users);
+  const [updatedUsers, setUpdatedUsers] = useState<User[]>([]);
 
-	const disabledUser = async (user: user) => {
-		const disabled = await deleteUser(Number(user.id));
-		console.log(disabled);
-		const updatedUsersList = updatedUsers.map((u) => {
-			if (u.id === user.id) {
-				return { ...u, active: disabled.user.active };
-			}
-			return u;
-		});
-		setUpdatedUsers(updatedUsersList);
-	};
+  const disabledUser = async (user: user) => {
+    const disabled = await deleteUser(Number(user.id));
+    console.log(disabled);
+    const updatedUsersList = updatedUsers.map((u) => {
+      if (u.id === user.id) {
+        return { ...u, active: disabled.user.active };
+      }
+      return u;
+    });
+    setUpdatedUsers(updatedUsersList);
+  };
 
-	useEffect(() => {
-		setUpdatedUsers(users);
-	}, [users]);
+  useEffect(() => {
+    setUpdatedUsers(users);
+  }, [users]);
 
-	return (
-		<table className="user-table">
-			<thead>
-				<tr>
-					<th className="user-table-th">ID</th>
-					<th className="user-table-th">Nombre</th>
-					<th className="user-table-th">Email</th>
-					<th className="user-table-th">Action</th>
-				</tr>
-			</thead>
-			<tbody>
-				{updatedUsers.map((user) => (
-					<tr key={user.id}>
-						<td className="user-table-td">{user.id}</td>
-						<td className="user-table-td">{user.fullName}</td>
-						<td className="user-table-td">{user.email}</td>
-						<td className="user-table-td">
-							<button onClick={() => disabledUser(user)}>
-								{user.active ? "Desactivar" : "Activar"}
-							</button>
+  return (
+    <div className="userTable">
+      <table className="user-table">
+        <thead>
+          <tr>
+            <th className="user-table-th">ID</th>
+            <th className="user-table-th">Nombre</th>
+            <th className="user-table-th">Email</th>
+            <th className="user-table-th">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {updatedUsers.map((user) => (
+            <tr key={user.id}>
+              <td className="user-table-td">{user.id}</td>
+              <td className="user-table-td">{user.fullName}</td>
+              <td className="user-table-td">{user.email}</td>
+              <td className="user-table-td">
+                <button onClick={() => disabledUser(user)}>
+                  {user.active ? "Desactivar" : "Activar"}
+                </button>
 
-							<Link to={`/user/${user.id}`}>
-								<button>Editar</button>
-							</Link>
-						</td>
-					</tr>
-				))}
-			</tbody>
-		</table>
-	);
+                <Link to={`/user/${user.id}`}>
+                  <button>Editar</button>
+                </Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Users;
