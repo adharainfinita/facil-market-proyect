4b9c9eb0747b30602792a8ad49006e46870304d4
hector141 <hectorcardoso18@outlook.com>
2023-07-07 18:45:59 -0300

    (client/FAQs):create faqs

---
+++ client/src/pages/FAQs.tsx
@@ -0,0 +1,37 @@
+
+function Faqs() {
+  return (
+  <div className="faqs_cont">
+    <h2 className="faqs_h2">Preguntas Frecuentes</h2>
+    <div className="faqs_container">
+        <div className="faqs_questions">
+            <h2>🤓👆¿Cómo se hacen los envios en Facil Marker?</h2>
+            <p>Los envios van completamente a cargo del vendedor. Se ahce un acuerdo entre vendedor y comprador. Nosotros no nos encargamos de esa parte.</p>
+        </div>
+        <div className="faqs_questions">
+        <h2>🤓👆¿Cómo puedo realizar el pago de manera segura?</h2>
+            <p>Ofrecemos un proceso de pago seguro y eficiente a través de Mercado Pago. Una vez que hayas elegido el producto
+            que deseas comprar, podrás realizar el pago de manera segura con diferentes métodos de pago.</p>
+        </div>
+        <div className="faqs_questions">
+        <h2>🤓👆¿Cómo puedo registrarme como vendedor en la plataforma?</h2>
+            <p>Para registrarte como vendedor, debes completar el formulario de registro. Una vez registrado, podrás crear y publicar tus productos.</p>
+        </div>
+        <div className="faqs_questions">
+        <h2>🤓👆¿Qué hago si no me llega el dinero?</h2>
+            <p>Primero intenta comunicarte con el comprador. Si eso no funciona manda un mail con tu problema a MarkerFacil@outlook.com.</p>
+        </div>
+        <div className="faqs_questions">
+        <h2>🤓👆¿Qué hago si quiero cancelar una venta?</h2>
+            <p>Para cancelar la venta tendras que comunicarte con nosotros en MarkerFacil@outlook.com, danos todos los datos de tu compra y nos comunicaremos en la brevedad.</p>
+        </div>
+        <div className="faqs_questions">
+        <h2>🤓👆¿Qué hago si el producto vino fallado?</h2>
+            <p>Si el producto te vino fallado o roto. Tendras que comunicarte con nosotros a traves de MarkerFacil@outlook.com y explicarnos la situacion con pruebas para que podamos resolverlo.</p>
+        </div>
+    </div>
+  </div>
+  );
+}
+
+export default Faqs;
